8bcca16b73294af1157a1c1999ee0dfa9d17f076
Owner's iMac <ownersimac@ownersimacsimac.attlocal.net>
2016-01-31 20:27:59 -0600

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Owner's iMac on 1/31/16.
+//  Copyright © 2016 Owner's iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
